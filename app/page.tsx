a93b0eade783287ebdf57f5f5a42db46facc200d
Hernando Abella <hernandoabella@gmail.com>
2025-12-13 23:32:52 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import Faq from "./components/Faq";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Testimonials from "./components/Testimonials";
-import WhatMakesItDifferent from "./components/WhatMakesItDifferent";
-import WhatYouWillLearn from "./components/WhatYouWillLearn";
 
 export default function Home() {
   return (
